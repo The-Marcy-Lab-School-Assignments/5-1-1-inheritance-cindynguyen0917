86900ee2a53e3c49196e918b8d95e305393d135c
Cindy Nguyen <cnguyen5138@gmail.com>
2024-07-14 17:33:35 -0400

    did the person class

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -33,7 +33,41 @@ class Square extends Rectangle {
 
 /* Be creative with this one! */
 class Person {
+  static #instances = []
+  constructor(name, partner, age) {
+    this.name = name;
+    this.partner = partner;
+    this.age = age;
+    Person.#instances.push(this)
+  }
+  getBirthYear() {
+    const currentYear = 2024;
+    return currentYear - this.age;
+  }
 
+  breakUp() {
+    if (!this.partner) {
+      return `${this.name} doesn't even have a partner...`
+    } else {
+      const oldPartner = this.partner;
+      this.partner = null;
+      return `${this.name} has broken up with ${oldParnter}.`
+    }
+  }
+  giveASummary() {
+    if (!this.partner && this.age > 18)
+      return `${this.name} is ${this.age} and is very lonely...`
+    else if (!this.partner)
+      return `${this.name} isn't allowed to date because they are only ${this.age}.`
+    else
+      return `${this.name} is ${this.age} and they are dating ${this.partner}.`
+  }
+  static listInstances() {
+    return [...Person.#instances]
+  }
+  static findByName(name) {
+    return Person.#instances.find((person) => person.name === name);
+  }
 }
 
 module.exports = {
